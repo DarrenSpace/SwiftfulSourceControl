a8272132bec5f3e3a4974833f0562974f6cf86bd
Darren Pan <darrenspace@icloud.com>
2025-08-27 22:17:03 +0800

    [Feature] Add title to HomeView

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -4,6 +4,25 @@
 //
 //  Created by Darren Pan on 8/27/25.
 //
+/*
+ COMMIT MESSAGES
+ 
+ New Feature:
+ [Feature] Description of the feature
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Description of changes
+ 
+ RELEASE:
+ [Release] Description of release
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+  
+ MUNDANE TASKS:
+ [Clean] Description of changes
+ 
+ */
 
 import SwiftUI
 
@@ -19,6 +38,8 @@ struct ContentView: View {
                 
             }
             .background(Color.red)
+            
+            Rectangle()
         }
         .padding()
     }
